fed9b26e05888ca3649a63e29ba3f2ec461538ee
olzhas <serikbayev97@gmail.com>
2023-03-14 19:33:10 +0600

    spring config server locally works!

--- vehicle-service/src/main/java/kz/dits/vehicleservice/VehicleServiceApplication.java
+++ vehicle-service/src/main/java/kz/dits/vehicleservice/VehicleServiceApplication.java
@@ -1,5 +1,6 @@
 package kz.dits.vehicleservice;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,11 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 @SpringBootApplication
 @RestController
 public class VehicleServiceApplication {
+    @Value("${hello}")
+    private String hello;
+    @Value("${shared}")
+    private String shared;
 
-    @GetMapping
+    @GetMapping("/hello")
     public String hello() {
-        return "Hello from vehicle-service!";
+        return hello;
     }
+
+    @GetMapping("/shared")
+    public String shared() { return shared;}
 
     public static void main(String[] args) {
         SpringApplication.run(VehicleServiceApplication.class, args);
