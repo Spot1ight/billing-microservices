cbf65186554be26141de034ae8c3917284dbe840
olzhas <serikbayev97@gmail.com>
2023-03-24 17:40:27 +0600

    eureka server added

---
+++ eureka-server/src/test/java/kz/dits/eurekaserver/EurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.dits.eurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
